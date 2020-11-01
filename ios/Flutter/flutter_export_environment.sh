62eda333383cb8d058db83b0509574fd0249085f
pawiie <pawan5653989@gmail.com>
2020-11-01 11:32:06 +0530

    added cards

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/pawansinghchauhan/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/pawansinghchauhan/AndroidStudioProjects/mi_card_flutter"
-export "FLUTTER_TARGET=/Users/pawansinghchauhan/AndroidStudioProjects/mi_card_flutter/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/pawansinghchauhan/AndroidStudioProjects/profileCard"
+export "FLUTTER_TARGET=/Users/pawansinghchauhan/AndroidStudioProjects/profileCard/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,9 +14,6 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            //crossAxisAlign
-
-            //crossAxisAlignment: CrossAxisAlignment.,
             children: [
               CircleAvatar(
                 radius: 70,
@@ -30,19 +28,56 @@ class MyApp extends StatelessWidget {
                     fontFamily: "Pacifico"),
               ),
               Text(
-                "FLUTTER  DEVELOPER",
+                "FLUTTER DEVELOPER",
                 style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                   fontFamily: "SourceSansPro",
+                  color: Colors.teal[100],
+                  letterSpacing: 2.5,
                 ),
               ),
               SizedBox(
-                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
               ),
-              Container(
-                color: Colors.white,
-                height: 30,
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal[900],
+                    size: 30,
+                  ),
+                  title: Text(
+                    "+91 9711691117",
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 30,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    "pawan5653989@gmail.com",
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
